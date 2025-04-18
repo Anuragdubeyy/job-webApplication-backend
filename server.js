41ce904ee38a91c78ce4d18eaacbb335f3ee1115
AnuragDubey67 <101169657+AnuragDubey67@users.noreply.github.com>
2025-04-18 22:34:39 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/userRoute');
+const { default: mongoose } = require('mongoose');
+
+dotenv.config();
+
+const MONGO = process.env.DB_PASSWORD;
+mongoose
+  .connect(
+    `mongodb+srv://anuragdubey16017:${MONGO}@crm.e6a4o.mongodb.net/CRM`,
+  )
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+
+const PORT =  5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
