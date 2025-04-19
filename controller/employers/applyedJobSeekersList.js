85b0fb455a9cd35348378a6d00011771ff667367
AnuragDubey67 <101169657+AnuragDubey67@users.noreply.github.com>
2025-04-19 18:45:31 +0530

    apply job Seekers list API

--- controller/employers/applyedJobSeekersList.js
+++ controller/employers/applyedJobSeekersList.js
@@ -1,31 +1,37 @@
-const job = require("../../model/job");
+const Job = require("../../model/job");
+const Application = require("../../model/application"); // Make sure you have this model
 
-const getJobApplicants = async (req, res) => {
+const getJobApplicants = async (req, res, next) => {
+  try {
     // Check if job exists and belongs to this employer
-    const job = await Job.findOne({
+    const existingJob = await Job.findOne({
       _id: req.params.jobId,
       employer: req.user.id
     });
-  
-    if (!job) {
+
+    if (!existingJob) {
       return next(
-        new ErrorResponse(`No job found with ID ${req.params.jobId}`, 404)
+        `No job found with ID ${req.params.jobId}`, 404
       );
     }
-  
+
     // Get all applications for this job with applicant details
     const applications = await Application.find({ job: req.params.jobId })
       .populate({
         path: 'applicant',
-        select: 'name email' // Only include name and email
+        select: 'name email mobile resume' // Include relevant fields
       })
-      .select('-job'); // Exclude job field from response
-  
+      .select('-job -__v'); // Exclude unnecessary fields
+
     res.status(200).json({
       success: true,
       count: applications.length,
       data: applications
     });
-  };
 
-module.exports = getJobApplicants
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getJobApplicants };
