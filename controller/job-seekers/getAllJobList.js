08c03a01b86a88f8da9f8c8471d4507c7d0d159e
AnuragDubey67 <101169657+AnuragDubey67@users.noreply.github.com>
2025-04-20 14:51:35 +0530

    update

--- controller/job-seekers/getAllJobList.js
+++ controller/job-seekers/getAllJobList.js
@@ -1,111 +1,26 @@
-// const job = require("../../model/job");
 
-// const getAllJobs = async (req, res) => {
-//     // Advanced filtering, sorting, pagination
-//     let query;
-  
-//     // Copy req.query
-//     const reqQuery = { ...req.query };
-  
-//     // Fields to exclude
-//     const removeFields = ['select', 'sort', 'page', 'limit'];
-  
-//     // Loop over removeFields and delete them from reqQuery
-//     removeFields.forEach(param => delete reqQuery[param]);
-  
-//     // Create query string
-//     let queryStr = JSON.stringify(reqQuery);
-  
-//     // Create operators ($gt, $gte, etc)
-//     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-  
-//     // Finding resource
-//     query = job.find(JSON.parse(queryStr)).populate({
-//       path: 'employer',
-//       select: 'name company'
-//     });
-  
-//     // Select Fields
-//     if (req.query.select) {
-//       const fields = req.query.select.split(',').join(' ');
-//       query = query.select(fields);
-//     }
-  
-//     // Sort
-//     if (req.query.sort) {
-//       const sortBy = req.query.sort.split(',').join(' ');
-//       query = query.sort(sortBy);
-//     } else {
-//       query = query.sort('-createdAt');
-//     }
-  
-//     // Pagination
-//     const page = parseInt(req.query.page, 10) || 1;
-//     const limit = parseInt(req.query.limit, 10) || 25;
-//     const startIndex = (page - 1) * limit;
-//     const endIndex = page * limit;
-//     const total = await Job.countDocuments();
-  
-//     query = query.skip(startIndex).limit(limit);
-  
-//     // Executing query
-//     const jobs = await query;
-  
-//     // Pagination result
-//     const pagination = {};
-  
-//     if (endIndex < total) {
-//       pagination.next = {
-//         page: page + 1,
-//         limit
-//       };
-//     }
-  
-//     if (startIndex > 0) {
-//       pagination.prev = {
-//         page: page - 1,
-//         limit
-//       };
-//     }
-  
-//     res.status(200).json({
-//       success: true,
-//       count: jobs.length,
-//       pagination,
-//       data: jobs
-//     });
-//   };
-  
-//   module.exports = {getAllJobs};
 const job = require("../../model/job");
 
 const getAllJobs = async (req, res, next) => {
   try {
-    // Advanced filtering, sorting, pagination
     let query;
 
-    // Copy req.query
     const reqQuery = { ...req.query };
 
-    // Fields to exclude
     const removeFields = ['select', 'sort', 'page', 'limit'];
 
-    // Loop over removeFields and delete them from reqQuery
     removeFields.forEach(param => delete reqQuery[param]);
 
-    // Create query string
     let queryStr = JSON.stringify(reqQuery);
 
     // Create operators ($gt, $gte, etc)
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
-    // Finding resource
     query = job.find(JSON.parse(queryStr)).populate({
       path: 'employer',
       select: 'name company'
     });
 
-    // Select Fields
     if (req.query.select) {
       const fields = req.query.select.split(',').join(' ');
       query = query.select(fields);
@@ -128,10 +43,8 @@ const getAllJobs = async (req, res, next) => {
 
     query = query.skip(startIndex).limit(limit);
 
-    // Executing query
     const jobs = await query;
 
-    // Pagination result
     const pagination = {};
 
     if (endIndex < total) {
